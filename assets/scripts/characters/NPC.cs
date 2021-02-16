385887bbfbf53d68c894987366e67a4b1efe834b
VaJau2 <vadim206-1998@yandex.ru>
2021-02-16 19:47:52 +0500

    npc bag spawn

--- assets/scripts/characters/NPC.cs
+++ assets/scripts/characters/NPC.cs
@@ -1,16 +1,30 @@
 using Godot;
+using Godot.Collections;
 
 //класс отвечает за поведение НПЦ
 public class NPC : Character
 {
     const int RAGDOLL_IMPULSE = 700;
-    NPCFace head;
-    AnimationPlayer anim;
-    AudioStreamPlayer3D audi;
-    Skeleton skeleton;
-    PhysicalBone headBone;
-    PhysicalBone bodyBone;
-    bool tempShotgunShot; //для увеличения импульса при получении урона от дробовика
+
+    [Export]
+    public Array<string> itemCodes = new Array<string>();
+    [Export]
+    public Dictionary<string, int> ammoCount = new Dictionary<string, int>();
+    [Export]
+    public bool SpawnRandomItems;
+
+    [Export]
+    public int StartHealth = 100;
+
+    private NPCFace head;
+    private AnimationPlayer anim;
+    private  AudioStreamPlayer3D audi;
+    private Skeleton skeleton;
+    private PhysicalBone headBone;
+    private PhysicalBone bodyBone;
+    private bool tempShotgunShot; //для увеличения импульса при получении урона от дробовика
+
+    private PackedScene bagPrefab;
 
     public override void TakeDamage(Character damager, int damage, int shapeID = 0)
     {
@@ -20,6 +34,10 @@ public class NPC : Character
 
         base.TakeDamage(damager, damage, shapeID);
         if (Health <= 0) {
+            if (itemCodes.Count > 0) {
+                SpawnItemsBag();
+            }
+
             AnimateDealth(damager, shapeID);
         }
     }
@@ -27,6 +45,19 @@ public class NPC : Character
     public override void CheckShotgunShot(bool isShotgun)
     {
         tempShotgunShot = isShotgun;
+    }
+
+    private void SpawnItemsBag()
+    {
+        FurnChest tempBag = (FurnChest)bagPrefab.Instance();
+
+        tempBag.itemCodes = itemCodes;
+        tempBag.ammoCount = ammoCount;
+
+        Node parent = GetNode("/root/Main/Scene");
+        parent.AddChild(tempBag);
+        tempBag.Translation = Translation;
+        tempBag.Translate(Vector3.Up / 4f);
     }
 
     private async void AnimateDealth(Character killer, int shapeID)
@@ -55,9 +86,15 @@ public class NPC : Character
         head = GetNode<NPCFace>("Armature/Skeleton/Body");
         headBone = GetNode<PhysicalBone>("Armature/Skeleton/Physical Bone neck");
         bodyBone = GetNode<PhysicalBone>("Armature/Skeleton/Physical Bone back_2");
+        bagPrefab = GD.Load<PackedScene>("res://objects/props/furniture/bag.tscn");
 
         anim.Play("Idle");
-        SetStartHealth(100);
+        SetStartHealth(StartHealth);
+
+        if (SpawnRandomItems) {
+            RandomItems items = GetNode<RandomItems>("/root/Main/Scene/randomItems");
+            items.LoadRandomItems(itemCodes, ammoCount);
+        }
     }
 
     public override void _Process(float delta)
