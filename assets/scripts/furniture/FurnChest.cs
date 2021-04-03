f04dd5c1c4006c93f08aaaf35874d509bafad4fb
VaJau2 <vadim206-1998@yandex.ru>
2021-04-03 14:24:52 +0500

    loading chest data

--- assets/scripts/furniture/FurnChest.cs
+++ assets/scripts/furniture/FurnChest.cs
@@ -1,7 +1,9 @@
+using System;
 using Godot;
 using Godot.Collections;
+using Array = Godot.Collections.Array;
 
-public class FurnChest: FurnBase {
+public class FurnChest: FurnBase, ISavable {
 
     InventoryMenu menu;
     [Export]
@@ -71,5 +73,79 @@ public class FurnChest: FurnBase {
     {
         base.ClickFurn();
         menu.Disconnect("MenuIsClosed", this, nameof(CloseFurn));
+    }
+
+    public Dictionary GetSaveData()
+    {
+        //переводим массив патронных кнопок в массив имен кнопок
+        var ammoButtonNames = new Dictionary<string, string>(); 
+        foreach (string ammoType in ammoButtons.Keys)
+        {
+            string buttonName = ammoButtons[ammoType].Name;
+            ammoButtonNames.Add(ammoType, buttonName);
+        }
+        
+        return new Dictionary()
+        {
+            {"parent", GetParent().Name},
+            {"fileName", Filename},
+            
+            {"pos_x", GlobalTransform.origin.x},
+            {"pos_y", GlobalTransform.origin.y},
+            {"pos_z", GlobalTransform.origin.z},
+            {"rot_x", GlobalTransform.basis.GetEuler().x},
+            {"rot_y", GlobalTransform.basis.GetEuler().y},
+            {"rot_z", GlobalTransform.basis.GetEuler().z},
+            
+            {"itemCodes", itemCodes},
+            {"ammoCount", ammoCount},
+            {"ammoButtons", ammoButtonNames},
+            {"itemPositions", itemPositions},
+            {"moneyCount", moneyCount},
+        };
+    }
+
+    public void LoadData(Dictionary data)
+    {
+        Vector3 newPos = new Vector3(Convert.ToSingle(data["pos_x"]), Convert.ToSingle(data["pos_y"]), Convert.ToSingle(data["pos_z"]));
+        Vector3 newRot = new Vector3(Convert.ToSingle(data["rot_x"]), Convert.ToSingle(data["rot_y"]), Convert.ToSingle(data["rot_z"]));
+
+        Basis newBasis = new Basis(newRot);
+        Transform newTransform = new Transform(newBasis, newPos);
+        GlobalTransform = newTransform;
+        
+        Array newItemCodes = (Array) data["itemCodes"];
+        Dictionary newAmmoCount = (Dictionary) data["ammoCount"];
+        Dictionary newAmmoButtonNames = (Dictionary) data["ammoButtons"];
+        Dictionary newItemPositions = (Dictionary) data["itemPositions"];
+
+        moneyCount = Convert.ToInt32(data["moneyCount"]);
+        
+        itemCodes.Clear();
+        foreach (string itemCode in newItemCodes)
+        {
+            itemCodes.Add(itemCode);
+        }
+
+        ammoCount.Clear();
+        foreach (string key in newAmmoCount.Keys)
+        {
+            ammoCount.Add(key, Convert.ToInt32(newAmmoCount[key]));
+        }
+        
+        ammoButtons.Clear();
+        var scene = GetNode("/root/Main/Scene");
+        foreach (string key in newAmmoButtonNames.Keys)
+        {
+            ItemIcon tempButton = (ItemIcon)Global.FindNodeInScene(scene, newAmmoButtonNames[key].ToString());
+            ammoButtons.Add(key, tempButton);
+        }
+        
+        itemPositions.Clear();
+        foreach (string key in newItemPositions.Keys)
+        {
+            int intKey = Convert.ToInt32(key);
+            itemPositions.Add(intKey, newItemPositions[key].ToString());
+        }
     }
 }
