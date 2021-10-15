d87815a580c5658f03276d7fbee65e1b3bc16ba6
VaJau2 <vadim206-1998@yandex.ru>
2021-10-15 20:12:03 +0500

    bellgates base stealth buck

--- assets/scripts/interface/inventory/effects/effectTypes/StealthBuckEffect.cs
+++ assets/scripts/interface/inventory/effects/effectTypes/StealthBuckEffect.cs
@@ -9,30 +9,76 @@ public class StealthBuckEffect : Effect
         badEffect = false;
     }
 
-    private void ChangeMeshVisibility(MeshInstance meshInstance, bool visible)
+    // проходит по первым дочерним элементам и имет там меш
+    private static MeshInstance FindMeshesInParent(Spatial parent)
+    {
+        while (true)
+        {
+            if (parent == null || parent.GetChildCount() <= 0)
+            {
+                return null;
+            }
+            
+            parent = parent.GetChild(0) as Spatial;
+
+            if (parent is MeshInstance instance)
+            {
+                return instance;
+            }
+        }
+    }
+
+    private static bool CanChangeTransparency(Resource material)
+    {
+        return !material.ResourcePath.EndsWith("wings.material");
+    }
+
+    private static void ChangeMeshVisibility(MeshInstance meshInstance, bool visible)
     {
         Mesh mesh = meshInstance.Mesh;
         int materialsCount = mesh.GetSurfaceCount();
         for (int i = 0; i < materialsCount; i++)
         {
             SpatialMaterial tempMaterial = mesh.SurfaceGetMaterial(i) as SpatialMaterial;
-            if (tempMaterial != null)
+            if (tempMaterial == null) continue;
+            if (CanChangeTransparency(tempMaterial))
             {
                 tempMaterial.FlagsTransparent = !visible;
-                Color old = tempMaterial.AlbedoColor;
-                tempMaterial.AlbedoColor = new Color(old.r, old.g, old.b, visible ? 1 : 0.02f);
             }
+            
+            Color old = tempMaterial.AlbedoColor;
+            tempMaterial.AlbedoColor = new Color(old.r, old.g, old.b, visible ? 1 : 0.02f);
         }
     }
 
     private void ChangePlayerVisibility(bool visible)
     {
         var bodyMesh = player.GetNode<MeshInstance>("player_body/Armature/Skeleton/Body");
-        var bodyThirdMesh = player.GetNode<MeshInstance>("player_body/Armature/Skeleton/Body_third");
+        //вернуть, если у гг от 3 лица есть материалы, которых нет у гг от 1 лица
+        // var bodyThirdMesh = player.GetNode<MeshInstance>("player_body/Armature/Skeleton/Body_third");
 
         ChangeMeshVisibility(bodyMesh, visible);
-        ChangeMeshVisibility(bodyThirdMesh, visible);
+        // ChangeMeshVisibility(bodyThirdMesh, visible);
+        
+        //смена прозрачности для оружия
+        if (player.Weapons.GunOn)
+        {
+            var weaponParent = player.GetWeaponParent(player.Weapons.isPistol);
+            var gunArmature = Global.FindNodeInScene(weaponParent, "Gun-armature") as Spatial;
+            var weaponMesh = FindMeshesInParent(gunArmature);
+            if (weaponMesh != null)
+            {
+                ChangeMeshVisibility(weaponMesh, visible);
+            }
 
+            if (!player.Weapons.isPistol)
+            {
+                var weaponBag = player.GetNode<MeshInstance>("player_body/Armature/Skeleton/BoneAttachment 2/shotgunBag");
+                ChangeMeshVisibility(weaponBag, visible);
+            }
+        }
+
+        //смена прозрачности у "тени крыльев" для пегаса
         if (Global.Get().playerRace != Race.Pegasus) return;
         
         var wingsShadow = player.GetNode<MeshInstance>("player_body/Armature/Skeleton/Wings");
