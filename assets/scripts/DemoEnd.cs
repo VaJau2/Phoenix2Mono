c6c258be2869c0b6ec730673ca5ffb29767547a7
VaJau2 <vadim206-1998@yandex.ru>
2021-12-29 21:03:19 +0500

    bug fixes

--- assets/scripts/DemoEnd.cs
+++ assets/scripts/DemoEnd.cs
@@ -4,10 +4,8 @@ public class DemoEnd : Control
 {
     public override void _Ready()
     {
-        var label1 = GetNode<Label>("Label");
         var label2 = GetNode<Label>("Label2");
         var label3 = GetNode<Label>("Label3");
-        label1.Text = InterfaceLang.GetPhrase("inGame", "labels", "endLabel1");
         label2.Text = InterfaceLang.GetPhrase("inGame", "labels", "endLabel2");
         label3.Text = InterfaceLang.GetPhrase("inGame", "labels", "endLabel3");
     }
